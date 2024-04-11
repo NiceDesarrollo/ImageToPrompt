ac78871fd1162c7e49c4b1bc5fe14278acd5c352
Gabriel Ahumada <gahumada@nice.com.mx>
2024-04-11 17:48:06 -0600

    api v1

---
+++ src/app/api/image-upload/route.ts
@@ -0,0 +1,56 @@
+import { NextResponse } from "next/server";
+import { File } from "formidable";
+import fs from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+
+export async function POST(request: Request) {
+  
+  const data = await request.formData();
+  const imageType = data.get("image").type || '';
+
+
+  const ImageRequestFile = data.get("image") as File;
+
+  // Define the path where the image will be stored
+  const imagePath = `./public/${ImageRequestFile.name}`;
+
+  // Use pipeline to handle backpressure and errors
+  await promisify(pipeline)(
+    ImageRequestFile.stream(),
+    fs.createWriteStream(imagePath)
+  );
+
+    // Access your API key as an environment variable (see "Set up your API key" above)
+    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI!);
+
+    function fileToGenerativePart(path:any, mimeType:string) {
+      return {
+        inlineData: {
+          data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+          mimeType
+        },
+      };
+    }
+  
+    // For text-and-image input (multimodal), use the gemini-pro-vision model
+    // For text-and-image input (multimodal), use the gemini-pro-vision model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+  
+    const prompt = "Could you provide me with the correct prompt to generate this  image?, please be carefull whith the colors, the background, the perspective, the style, and the quality";
+  
+    const imageParts = [
+      fileToGenerativePart(imagePath, imageType),
+    ];
+  
+    const result = await model.generateContent([prompt, ...imageParts]);
+    const response = await result.response;
+    const text = response.text();
+
+  
+
+  return NextResponse.json({ message: text }, { status: 200 });
+}
