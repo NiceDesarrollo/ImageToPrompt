e6c8f323b01a12adfd4f700a3c3a5efea2630eb4
Gabriel Ahumada <gahumada@nice.com.mx>
2024-04-15 17:18:40 -0600

    Reponse test url

--- src/app/api/image-upload/route.js
+++ src/app/api/image-upload/route.js
@@ -7,7 +7,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export async function POST(request) {
   const data = await request.formData();
-  return NextResponse.json({ message: data }, { status: 200 });
+  return NextResponse.json({ message: 'hello' }, { status: 200 });
 
   const imageType = data.get("image").type || "";
 
