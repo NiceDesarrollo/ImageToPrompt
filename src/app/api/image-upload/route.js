dca523a452d2ce6c362b370db0fdefaf5b3d8fe9
Gabriel Ahumada <gahumada@nice.com.mx>
2024-04-15 11:48:48 -0600

    Delete the image after the process

--- src/app/api/image-upload/route.js
+++ src/app/api/image-upload/route.js
@@ -1,17 +1,13 @@
 import { NextResponse } from "next/server";
 import { File } from "formidable";
-import fs from 'fs';
-import { pipeline } from 'stream';
-import { promisify } from 'util';
+import fs from "fs";
+import { pipeline } from "stream";
+import { promisify } from "util";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-
-
 export async function POST(request) {
-  
   const data = await request.formData();
-  const imageType = data.get("image").type || '';
-
+  const imageType = data.get("image").type || "";
 
   const ImageRequestFile = data.get("image");
 
@@ -24,33 +20,38 @@ export async function POST(request) {
     fs.createWriteStream(imagePath)
   );
 
-    // Access your API key as an environment variable (see "Set up your API key" above)
-    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI);
+  // Access your API key as an environment variable (see "Set up your API key" above)
+  const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI);
 
-    function fileToGenerativePart(path, mimeType) {
-      return {
-        inlineData: {
-          data: Buffer.from(fs.readFileSync(path)).toString("base64"),
-          mimeType
-        },
-      };
+  function fileToGenerativePart(path, mimeType) {
+    return {
+      inlineData: {
+        data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+        mimeType,
+      },
+    };
+  }
+
+  // For text-and-image input (multimodal), use the gemini-pro-vision model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+  const prompt =
+    "Could you provide me with the correct prompt to generate this  image?, please be carefull whith the colors, the background, the perspective, the style, the quality, and be the more descriptive you can";
+
+  const imageParts = [fileToGenerativePart(imagePath, imageType)];
+
+  const result = await model.generateContent([prompt, ...imageParts]);
+  const response = await result.response;
+  const text = response.text();
+
+  // Delete the image
+  fs.unlink(imagePath, (err) => {
+    if (err) {
+      console.error("Error deleting the image:", err);
+    } else {
+      console.log("Image deleted successfully");
     }
+  });
   
-    // For text-and-image input (multimodal), use the gemini-pro-vision model
-    // For text-and-image input (multimodal), use the gemini-pro-vision model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
-  
-    const prompt = "Could you provide me with the correct prompt to generate this  image?, please be carefull whith the colors, the background, the perspective, the style, the quality, and be the more descriptive you can";
-  
-    const imageParts = [
-      fileToGenerativePart(imagePath, imageType),
-    ];
-  
-    const result = await model.generateContent([prompt, ...imageParts]);
-    const response = await result.response;
-    const text = response.text();
-
-  
-
   return NextResponse.json({ message: text }, { status: 200 });
 }
