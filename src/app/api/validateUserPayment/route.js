e3d7e67af469b9e1b3519fcb86df3e4a0b0c8af9
Gabriel Ahumada <gahumada@nice.com.mx>
2024-05-02 15:36:47 -0600

    trying to solve url fetch error

--- src/app/api/validateUserPayment/route.js
+++ src/app/api/validateUserPayment/route.js
@@ -3,28 +3,33 @@ import UserPayment from "@/app/models/UserPayments";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
+  try {
+    const body = await request.text();
 
-  const body = await request.text();
+    // Convertir la cadena de texto a un objeto
+    const parsedBody = JSON.parse(body);
 
-  // Convertir la cadena de texto a un objeto
-  const parsedBody = JSON.parse(body);
+    // Acceder al valor de userEmail
+    const userEmail = parsedBody.userEmail;
 
-  // Acceder al valor de userEmail
-  const userEmail = parsedBody.userEmail;
+    await dbConnect();
 
-  await dbConnect();
+    const userFound = await UserPayment.findOne({ email: userEmail });
 
-  const userFound = await UserPayment.findOne({ email: userEmail });
-
-  if (!userFound) {
-    console.log("user not found in userPayment");
-    return NextResponse.json({ message: false }, { status: 200 });
-  } else {
-    if (userFound.canGetThePrompt) {
-      return NextResponse.json({ message: true }, { status: 200 });
+    if (!userFound) {
+      console.log("user not found in userPayment");
+      return NextResponse.json({ message: false }, { status: 200 });
     } else {
-      return NextResponse.json({ message: false }, { status: 404 });
+      if (userFound.canGetThePrompt) {
+        return NextResponse.json({ message: true }, { status: 200 });
+      } else {
+        return NextResponse.json({ message: false }, { status: 404 });
+      }
     }
+  } catch (error) {
+    console.error(error);
+    // Handle the error appropriately
+    // You might want to send a response with an error status code
+    return NextResponse.json({ message: "An error occurred" }, { status: 500 });
   }
-
 }
